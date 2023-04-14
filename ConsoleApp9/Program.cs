e2f87f12b7089e094e0967dedaa19a7eb1197b08
melika <melikatorabi031@gmail.com>
2023-04-14 20:18:53 +0430

    Add project files.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace project
+{
+    class test1
+    {
+        public static void Main()
+        {
+            int L=Convert.ToInt32(Console.ReadLine());
+            int H=Convert.ToInt32(Console.ReadLine());
+            int W=Convert.ToInt32(Console.ReadLine());
+            double show= Calc( L, H, W );
+            Console.WriteLine( show );
+        }//end Main
+        private static double Calc (int num1,int num2,int num3)
+        {
+            double Result= num1*num2*num3;
+            return Result;
+        }//end  function
+    }//end class
+
+}//end namespace
